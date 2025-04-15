608212d31104a528bbf383e51dd82697d6cd3092
Gard Knutsen-Sæbø <gard.sebo@nav.no>
2025-04-15 14:37:33 +0200

    Lagt til tester

--- skattetrekk-backend/src/test/java/no/nav/pensjon/selvbetjening/skattetrekk/endpoint/SkattetrekkController.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.pensjon.selvbetjening.skattetrekk.endpoint
-
-import org.springframework.test.context.junit.jupiter.SpringExtension
-
-@ExtendedWith(SpringExtension::class)
-class SkattetrekkController {
-}
