ca3bd1d14f9b8682d6c0c770f77bd329e1d1f36d
Gard Knutsen-Sæbø <gard.sebo@nav.no>
2025-04-16 08:06:22 +0200

    Import av jxt() i sikkerhetstest

--- skattetrekk-backend/src/test/java/no/nav/pensjon/selvbetjening/skattetrekk/security/SetPidFilterTest.kt
+++ skattetrekk-backend/src/test/java/no/nav/pensjon/selvbetjening/skattetrekk/security/SetPidFilterTest.kt
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
