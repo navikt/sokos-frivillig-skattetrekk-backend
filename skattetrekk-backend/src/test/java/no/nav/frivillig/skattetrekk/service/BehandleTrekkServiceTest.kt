0996b55db55e081e1ac3b1605d03191e1ae84b6e
Gard Knutsen-Sæbø <gard.sebo@nav.no>
2025-05-02 14:03:27 +0200

    Opprettet tester for opphør

--- skattetrekk-backend/src/test/java/no/nav/frivillig/skattetrekk/service/BehandleTrekkServiceTest.kt
+++ skattetrekk-backend/src/test/java/no/nav/frivillig/skattetrekk/service/BehandleTrekkServiceTest.kt
@@ -19,7 +19,7 @@ class BehandleTrekkServiceTest {
     private val behandleTrekkService = BehandleTrekkService(trekkClientMock, geografiskLokasjonServiceMock)
 
     @Test
-    fun `opprett nytt frivillig skattetrekk for prosent`() {
+    fun `Opprett nytt frivillig skattetrekk for prosent`() {
 
         every { geografiskLokasjonServiceMock.hentNavEnhet(pid) } returns "NAV Enhet"
         every { trekkClientMock.opprettAndreTrekk(eq(pid), any()) } returns OpprettAndreTrekkResponse(1L)
@@ -37,7 +37,7 @@ class BehandleTrekkServiceTest {
     }
 
     @Test
-    fun `opprett nytt frivillig skattetrekk for nok`() {
+    fun `Opprett nytt frivillig skattetrekk for nok`() {
 
         every { geografiskLokasjonServiceMock.hentNavEnhet(pid) } returns "NAV Enhet"
         every { trekkClientMock.opprettAndreTrekk(eq(pid), any()) } returns OpprettAndreTrekkResponse(1L)
@@ -55,7 +55,7 @@ class BehandleTrekkServiceTest {
     }
 
     @Test
-    fun `ikke opprett nytt frivillig skattetrekk dersom tilleggstrekk er 0`() {
+    fun `Ikke opprett nytt frivillig skattetrekk dersom tilleggstrekk er 0`() {
 
         val trekkvedtakId = behandleTrekkService.opprettTrekk(pid, 0, SatsType.KRONER)
 
@@ -73,34 +73,108 @@ class BehandleTrekkServiceTest {
         assertNull(trekkvedtakId)
     }
 
-    private fun lagFinnTrekkListe():List<TrekkInfo> = listOf(
-            TrekkInfo(
-                trekkvedtakId = null,
-                debitor = null,
-                trekktype = null,
-                trekkperiodeFom = null,
-                trekkperiodeTom = null,
-                trekkstatus = null,
-                kreditor = null,
-                kreditorRef = null,
-                tssEksternId = null,
-                trekkalternativ = null,
-                sats = null,
-                belopSaldotrekk = null,
-                belopTrukket = null,
-                ansvarligEnhetId = null,
+    @Test
+    fun `Ikke opphør noe dersom det bare er tomme satsperioder`() {
+        val trekkVedtakId = 1L
+
+        every { geografiskLokasjonServiceMock.hentNavEnhet(pid) } returns "NAV Enhet"
+        every { trekkClientMock.hentSkattOgTrekk(pid, trekkVedtakId) } returns lagHentSkattOgTrekkRespons(trekkVedtakId, emptyList())
+
+        behandleTrekkService.opphoerTrekk(pid, trekkVedtakId)
+
+        verify(exactly = 0) {
+            trekkClientMock.opphorAndreTrekk(
+                eq(pid),
+                any()
             )
+        }
+    }
+
+    @Test
+    fun `Opphør trekk med løpende satsperiode`() {
+        val trekkVedtakId = 1L
+
+        every { geografiskLokasjonServiceMock.hentNavEnhet(pid) } returns "NAV Enhet"
+        every { trekkClientMock.opphorAndreTrekk(eq(pid), any()) } returns Unit
+
+        every { trekkClientMock.hentSkattOgTrekk(pid, trekkVedtakId) } returns lagHentSkattOgTrekkRespons(trekkVedtakId,
+            listOf(lagSatsperiode(
+                fom = LocalDate.now().minusMonths(1L),
+                tom = LocalDate.now().plusMonths(3L),
+                sats = 100.0
+            ))
         )
+
+        behandleTrekkService.opphoerTrekk(pid, trekkVedtakId)
+
+        verify(exactly = 1) {
+            trekkClientMock.opphorAndreTrekk(
+                eq(pid),
+                any()
+            )
+        }
+    }
+    @Test
+    fun `Opphør trekk med fremtidig satsperiode`() {
+        val trekkVedtakId = 1L
+
+        every { geografiskLokasjonServiceMock.hentNavEnhet(pid) } returns "NAV Enhet"
+        every { trekkClientMock.opphorAndreTrekk(eq(pid), any()) } returns Unit
+
+        every { trekkClientMock.hentSkattOgTrekk(pid, trekkVedtakId) } returns lagHentSkattOgTrekkRespons(trekkVedtakId,
+            listOf(lagSatsperiode(
+                fom = LocalDate.now().plusMonths(1L),
+                tom = LocalDate.now().plusMonths(3L),
+                sats = 100.0
+            ))
+        )
+
+        behandleTrekkService.opphoerTrekk(pid, trekkVedtakId)
+
+        verify(exactly = 1) {
+            trekkClientMock.opphorAndreTrekk(
+                eq(pid),
+                any()
+            )
+        }
+    }
+
+    @Test
+    fun `Opphør trekk med både løpende og fremtidig satsperiode`() {
+        val trekkVedtakId = 1L
+
+        every { geografiskLokasjonServiceMock.hentNavEnhet(pid) } returns "NAV Enhet"
+        every { trekkClientMock.opphorAndreTrekk(eq(pid), any()) } returns Unit
+
+        every { trekkClientMock.hentSkattOgTrekk(pid, trekkVedtakId) } returns lagHentSkattOgTrekkRespons(trekkVedtakId,
+            listOf(
+                lagSatsperiode(
+                    fom = LocalDate.now().minusMonths(1L),
+                    tom = LocalDate.now().plusMonths(3L),
+                    sats = 100.0),
+                lagSatsperiode(
+                    fom = LocalDate.now().plusMonths(1L),
+                    tom = LocalDate.now().plusMonths(3L),
+                    sats = 100.0
+                ))
+        )
+
+        behandleTrekkService.opphoerTrekk(pid, trekkVedtakId)
+
+        verify(exactly = 2) {
+            trekkClientMock.opphorAndreTrekk(
+                eq(pid),
+                any()
+            )
+        }
+    }
 
 
     private fun lagSatsperiode(fom: LocalDate, tom: LocalDate, sats: Double): Satsperiode {
         return Satsperiode(fom, tom, BigDecimal.valueOf(sats), erFeilregistrert = false)
     }
 
-    private fun lagHentSkattOgTrekkRespons(
-        andreTrekkVedtakId: Long,
-        satsperiodeListe: List<Satsperiode>
-        ) = HentSkattOgTrekkResponse(
+    private fun lagHentSkattOgTrekkRespons(andreTrekkVedtakId: Long, satsperiodeListe: List<Satsperiode>) = HentSkattOgTrekkResponse(
         skattetrekk = null,
         andreTrekk = AndreTrekkResponse(
             trekkvedtakId = andreTrekkVedtakId,
