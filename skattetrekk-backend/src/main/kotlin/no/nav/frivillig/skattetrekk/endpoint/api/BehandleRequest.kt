65f2e793076c4351fd2b536661f94dbdcb3208bd
Gard Knutsen-Sæbø <gard.sebo@nav.no>
2025-05-08 09:04:03 +0200

    Små endringer

--- skattetrekk-backend/src/main/kotlin/no/nav/frivillig/skattetrekk/endpoint/api/BehandleRequest.kt
+++ skattetrekk-backend/src/main/kotlin/no/nav/frivillig/skattetrekk/endpoint/api/BehandleRequest.kt
@@ -1,2 +1,8 @@
 package no.nav.frivillig.skattetrekk.endpoint.api
 
+import no.nav.frivillig.skattetrekk.client.trekk.api.SatsType
+
+data class BehandleRequest (
+    val value: Int,
+    val satsType: SatsType
+)
