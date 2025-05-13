cf4cac25bcd018a3a489349783033ecce890e281
Gard Knutsen-Sæbø <gard.sebo@nav.no>
2025-05-13 14:45:52 +0200

    Fjernet kompleksitet i FrivilligSkattetrekkMessage

--- skattetrekk-backend/src/main/kotlin/no/nav/frivillig/skattetrekk/endpoint/api/FrivilligSkattetrekkMessage.kt
+++ skattetrekk-backend/src/main/kotlin/no/nav/frivillig/skattetrekk/endpoint/api/FrivilligSkattetrekkMessage.kt
@@ -1,14 +1,9 @@
 package no.nav.frivillig.skattetrekk.endpoint.api
 
 data class FrivilligSkattetrekkMessage(
-    val messageCode: FrivilligSkattettrekkMessageCode,
     val details: String? = null,
-    val type: FrivilligSkattetrekkType = messageCode.type,
+    val type: FrivilligSkattetrekkType,
 )
-
-enum class FrivilligSkattettrekkMessageCode(val type: FrivilligSkattetrekkType, val details: String) {
-    OPPDRAG_SYSTEMET_ER_NEDE(FrivilligSkattetrekkType.ERROR, "Oppdragsystemet er nede eller utilgjengelig"),
-}
 
 enum class FrivilligSkattetrekkType {
     ERROR,
