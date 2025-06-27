ca57b083c47b0319edf88bcf596347f6ad852fea
Juliana Coelho Maccarini Skrøvseth <juliana.coelho.maccarini.skrovseth@nav.no>
2025-06-27 13:45:26 +0200

    endre feilmelding

--- skattetrekk-frontend/src/components/pageStatus/ErrorMessage.tsx
+++ skattetrekk-frontend/src/components/pageStatus/ErrorMessage.tsx
@@ -6,7 +6,8 @@ export function ErrorMessage() {
     return (
         <>
             <div id="error-div">
-            <Alert variant="error">Noe gikk galt! Prøv igjen om noen minutter.</Alert>
+            <Alert variant="error">Det har skjedd en teknisk feil. Hvis du har registrert informasjon, har den dessverre ikke blitt lagret. Vi beklager for dette. Du kan prøve igjen senere.
+                Ta gjerne kontakt med oss hvis problemet fortsetter.</Alert>
             </div>
         </>
     )
