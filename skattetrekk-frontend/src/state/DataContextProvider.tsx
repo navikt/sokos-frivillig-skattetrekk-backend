f6decd248deb28d4d97c99039841b91580c0b1a1
henrikolsvik <henrik-olsvik@outlook.com>
2025-07-25 10:55:46 +0200

    Ny alt spinner

--- skattetrekk-frontend/src/state/DataContextProvider.tsx
+++ skattetrekk-frontend/src/state/DataContextProvider.tsx
@@ -1,6 +1,6 @@
 import React, {createContext, useCallback, useEffect, useState} from 'react'
 import {fetchSkattetrekk, FrivilligSkattetrekkData, FrivilligSkattetrekkResponse} from "@/api/skattetrekkBackendClient";
-import {BodyShort, Box, Loader, VStack} from "@navikt/ds-react";
+import {BodyShort, Box, Heading, Loader, VStack} from "@navikt/ds-react";
 
 interface DataContextValue {
     getResponse: FrivilligSkattetrekkResponse | null
@@ -51,6 +51,7 @@ function DataContextProvider(props: DataContextProviderProps) {
     }, [refetch, shouldRefetch])
 
     const showLoader = (getResponse === null || isFetching) && !loaderOverride;
+    const showKvitteringsideLoader = (getResponse === null || isFetching) && loaderOverride;
 
     return (
         <DataContext.Provider
@@ -63,6 +64,18 @@ function DataContextProvider(props: DataContextProviderProps) {
             <Box position="relative" minHeight={showLoader ? "400px" : undefined}>
                 {props.children}
 
+                {(showKvitteringsideLoader) ? (
+                    <Box background="bg-subtle" padding="16" borderRadius="large">
+                        <VStack align="center" gap="8">
+                            <Heading align="center" size={"large"} level="2">
+                                Vent mens vi sender inn
+                            </Heading>
+                            <Loader size="3xlarge" />
+                            <BodyShort align="center">Dette kan ta opptil ett minutt.</BodyShort>
+                        </VStack>
+                    </Box>
+                ) : null }
+
                 {(showLoader) ?
                     (
                         <Box position="absolute" left="0" right="0" top="0" bottom="0" background="bg-default" style={{ zIndex: 100 }}>
@@ -73,7 +86,6 @@ function DataContextProvider(props: DataContextProviderProps) {
                                 </VStack>
                             </Box>
                         </Box>
-
                     ) : null}
             </Box>
         </DataContext.Provider>
