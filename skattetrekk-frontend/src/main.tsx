0ef93257453e8f092b292097d299daa56c289738
henrikolsvik <henrik-olsvik@outlook.com>
2025-07-29 16:29:58 +0200

    Test

--- skattetrekk-frontend/src/main.tsx
+++ skattetrekk-frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from "@/App"
 import DataContextProvider from "@/state/DataContextProvider";
 import "@navikt/ds-css";
-import "./index.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
