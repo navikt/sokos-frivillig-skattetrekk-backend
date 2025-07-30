7f6964a90b0e637b69c5e18e32852b5eb6ede8cd
henrikolsvik <henrik-olsvik@outlook.com>
2025-07-30 09:57:08 +0200

    CSS endring fullmakt

--- skattetrekk-frontend/src/main.tsx
+++ skattetrekk-frontend/src/main.tsx
@@ -8,7 +8,7 @@ import "./App.css";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <DataContextProvider>
-          <div className="microfrontend-container content-wrapper">
+          <div className="microfrontend-container">
               <representasjon-banner
                   representasjonstyper="PENSJON_FULLSTENDIG,PENSJON_BEGRENSET,PENSJON_SKRIV,PENSJON_KOMMUNISER,PENSJON_LES,PENSJON_SAMHANDLER,PENSJON_SAMHANDLER_ADMIN,PENSJON_SUPERADMIN"
                   redirectTo={`${window.location.origin}/utbetaling/skattetrekk/`}
