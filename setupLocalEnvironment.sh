183fc4a552a85de0dffd2f7334dc07a1ffab26e9
takwaiwang <tak.wai.wang@nav.no>
2025-08-22 10:44:47 +0200

    legg til støtte for å kjøre opp med lokal miljø

---
+++ setupLocalEnvironment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ensure user is authenicated, and run login if not.
+gcloud auth print-identity-token &> /dev/null
+if [ $? -gt 0 ]; then
+    gcloud auth login
+fi
+kubectl config use-context dev-fss
+kubectl config set-context --current --namespace=okonomi
+
+# Get AZURE system variables
+envValue=$(kubectl exec -it $(kubectl get pods | grep sokos-frivillig-skattetrekk-backend-q2 | cut -f1 -d' ') -c sokos-frivillig-skattetrekk-backend-q2 -- env | egrep "^AZURE|^TREKK|^TOKEN_X" | sort)
+
+# Set AZURE as local environment variables
+rm -f defaults.properties
+echo "$envValue" > defaults.properties
+echo "AZURE stores as defaults.properties"
+
+rm -f privateKey
+echo "$PRIVATE_KEY" > privateKey
+
