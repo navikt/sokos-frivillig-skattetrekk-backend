8074cdd78a6c78d3036af68dac818b5efd351afa
mudasar187 <mudasar_cheema@hotmail.com>
2025-10-01 13:15:11 +0200

    rydde opp i avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,12 @@ import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.2.10"
-    kotlin("plugin.spring") version "2.2.10"
-    id("org.springframework.boot") version "3.5.5"
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.spring") version "2.2.20"
+    id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
-    id("org.jetbrains.kotlinx.kover") version "0.9.1"
+    id("org.jetbrains.kotlinx.kover") version "0.9.2"
 
     application
 }
@@ -20,14 +20,13 @@ repositories {
     mavenCentral()
 }
 
-val jacksonVersion = "2.19.2"
+val jacksonVersion = "2.20.0"
 val logstashVersion = "8.1"
 val logbackVersion = "1.5.18"
-val micrometerVersion = "1.15.3"
+val micrometerVersion = "1.15.4"
 val kotlinLoggingVersion = "3.0.5"
 val janionVersion = "3.1.12"
 val mockkVersion = "1.14.5"
-val mockWebServerVersion = "5.1.0"
 
 dependencies {
 
@@ -45,10 +44,7 @@ dependencies {
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
 
     // Jackson
-    implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:$jacksonVersion")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
 
     // Micrometer / Prometheus
     implementation("io.micrometer:micrometer-registry-prometheus:$micrometerVersion")
@@ -58,9 +54,7 @@ dependencies {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
 
-    testImplementation("org.springframework.security:spring-security-test")
     testImplementation("io.mockk:mockk-jvm:$mockkVersion")
-    testImplementation("com.squareup.okhttp3:mockwebserver:$mockWebServerVersion")
 }
 
 // Vulnerability fix because of id("org.jlleitschuh.gradle.ktlint") uses ch.qos.logback:logback-classic:1.3.5
